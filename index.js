dbb38f836469e6bdd8bcc2d114f5853efd1fdad6
TDev-GabrielNavarro <g.navarrodealba@hotmail.com>
2025-03-07 13:35:22 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+document.getElementById('clickableButton').addEventListener('click', function() {
+    document.getElementById('carta').style.display = 'none';
+    document.getElementById('clickableButton').style.display = 'none';
+    document.getElementById('text').style.display = 'block';
+});
